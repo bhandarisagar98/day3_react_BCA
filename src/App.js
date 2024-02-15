90b169de0c52fe5e5fa40373bab5fbb18f752608
Prajwal Sagar Bhandari <bhandarisagar98@gmail.com>
2024-02-15 11:27:11 +0545

    loading

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import Card from "./components/Card";
 
 function App() {
   const [data, setData] = useState([]);
-  const fetchBlog = async () => {
-    const res = await axios.get(
-      "https://65cd9528c715428e8b3e9af4.mockapi.io/blog"
-    );
-    console.log(res);
 
-    setData(res.data);
+  const [loading, setLoading] = useState(true);
+  const fetchBlog = async () => {
+    try {
+      const res = await axios.get(
+        "https://65cd9528c715428e8b3e9af4.mockapi.io/blog"
+      );
+      if (res.status === 200) {
+        setLoading(false);
+        setData(res.data);
+      }
+    } catch (error) {
+      setLoading(false);
+    }
   };
   useEffect(() => {
     fetchBlog();
@@ -25,9 +32,12 @@ function App() {
         padding: "30px",
       }}
     >
-      {data.map((row) => {
-        return <Card key={row.id} row={row} />;
-      })}
+      {loading
+        ? "loading..."
+        : data.map((row) => {
+            return <Card key={row.id} row={row} />;
+          })}
+      {}
     </div>
   );
 }
