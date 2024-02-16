b3f63e937ce91e6381a1be6938b891438dbda440
Prajwal Sagar Bhandari <bhandarisagar98@gmail.com>
2024-02-16 11:45:20 +0545

    push for layout

--- src/pages/admin/Create.jsx
+++ src/pages/admin/Create.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Create = () => {
-  return <div style={{ color: "red" }}>Create</div>;
+  const createBlog = async (e) => {
+    e.preventDefault();
+    let data = new FormData(e.currentTarget);
+
+    let formData = {
+      name: data.get("name"),
+      avatar: data.get("avatar"),
+      desc: data.get("desc"),
+    };
+    console.log(formData);
+  };
+  return (
+    <div style={{ color: "red" }}>
+      <form
+        onSubmit={createBlog}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          width: "50%",
+        }}
+      >
+        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="avatar" placeholder="avatar" />
+        <textarea type="text" name="desc" placeholder="desc" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
